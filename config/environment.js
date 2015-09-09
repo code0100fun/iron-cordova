20b3e8a0d88d63df014e5d71368ffcb616dd7577
Chase McCarthy <chase@code0100fun.com>
2015-09-09 01:18:03 -0400

    ember-ionic demo working

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,17 @@
 /* jshint node: true */
 
+var os     = require('os');
+var ifaces = os.networkInterfaces();
+
+var addresses = [];
+for (var dev in ifaces) {
+  ifaces[dev].forEach(function(details){
+    if(details.family === 'IPv4' && details.address !== '127.0.0.1') {
+      addresses.push(details.address);
+    }
+  });
+}
+
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'iron',
@@ -23,6 +35,7 @@ module.exports = function(environment) {
     cordova: {
       rebuildOnChange: false,
       emulate: false,
+      emberUrl: 'http://' + addresses[0] + ':4200',
       liveReload: {
         enabled: true,
         platform: 'ios'
@@ -36,12 +49,15 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     ENV.APP.LOG_VIEW_LOOKUPS = true;
+
+    ENV.apiUrl   = 'http://' + addresses[0] + ':3000/api/v1';
+    ENV.development = true;
   }
 
   if (environment === 'test') {
     // Testem prefers this...
     ENV.baseURL = '/';
-    ENV.locationType = 'none';
+    ENV.locationType = 'auto';
 
     // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
@@ -50,9 +66,26 @@ module.exports = function(environment) {
     ENV.APP.rootElement = '#ember-testing';
   }
 
-  if (environment === 'production') {
-
+  if (environment === 'staging') {
+    ENV.apiUrl = 'http://iron-staging.herokuapp.com/api/v1';
+    ENV.staging = true;
   }
+
+
+  if (environment === 'production') {
+    ENV.apiUrl = 'http://iron.herokuapp.com/api/v1';
+    ENV.production = true;
+  }
+
+  ENV.contentSecurityPolicy = {
+    'default-src': "'unsafe-inline'",
+    'script-src': "'self' 'unsafe-eval' 'unsafe-inline'",
+    'style-src': "'self' 'unsafe-inline'",
+    'connect-src': "'self'",
+    'img-src': "'self' data:",
+    'font-src': "'self'",
+    'media-src': "'self'"
+  };
 
   return ENV;
 };
